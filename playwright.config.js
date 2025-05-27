29898d7bf62962604728a422b75316dd1fa5aa08
Anusree Eluri <anusree.e@tezo.com>
2025-05-27 17:07:53 +0530

    updated

--- playwright.config.js
+++ playwright.config.js
@@ -7,19 +7,19 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   reporter: 'html',
-  timeout: 120000,
+  timeout: 0,
 //  workers: 1,
  // workers: process.env.CI ? 2 : undefined,
  workers: process.env.CI ? 1 : undefined,
 
   expect: {
-    timeout: 120000, // ✅ 10 seconds for all expect() calls
+    timeout: 0,
   },
 
   use: {
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
-    headless: true,
+    headless: false,
   },
 
   projects: [
@@ -27,13 +27,13 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
   ],
 });
