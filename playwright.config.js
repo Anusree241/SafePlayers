3ce5148894fc22cab8c883f3b78cd675ebeab414
Varshitha Gurram <varshitha.g@tezo.com>
2025-05-28 12:44:16 +0530

    ClubContact Invites

--- playwright.config.js
+++ playwright.config.js
@@ -1,4 +1,8 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+// <<<<<<< HEAD
+>>>>>>> 0a25a54 (ClubContact Invite)
 import { defineConfig, devices } from '@playwright/test';
  
 export default defineConfig({
@@ -7,11 +11,11 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   reporter: 'html',
-  timeout: 180000,
+  timeout: 0,
  // workers: 1,
  workers: process.env.CI ? 1 : undefined,
   expect: {
-    timeout: 20000, 
+    timeout: 0 
   },
  
   use: {
@@ -19,9 +23,14 @@ export default defineConfig({
     screenshot: 'only-on-failure',
   },
  
+<<<<<<< HEAD
 =======
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
+=======
+// @ts-check
+// import { defineConfig, devices } from '@playwright/test';
+>>>>>>> 0a25a54 (ClubContact Invite)
 
 /**
  * Read environment variables from file.
@@ -32,6 +41,7 @@ import { defineConfig, devices } from '@playwright/test';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
+<<<<<<< HEAD
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
@@ -122,3 +132,95 @@ export default defineConfig({
 });
 
 >>>>>>> origin/master
+=======
+//  * @see https://playwright.dev/docs/test-configuration
+//  */
+// export default defineConfig({
+//   testDir: './tests',
+//   /* Run tests in files in parallel */
+//   fullyParallel: true,
+//   /* Fail the build on CI if you accidentally left test.only in the source code. */
+//   forbidOnly: !!process.env.CI,
+//   /* Retry on CI only */
+//   retries: process.env.CI ? 2 : 0,
+//   /* Opt out of parallel tests on CI. */
+//   workers: process.env.CI ? 1 : undefined,
+//   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+//   reporter: 'html',
+//   timeout:120000,
+
+//   expect: {
+//     timeout: 120000, // ✅ 10 seconds for all expect() calls
+//   },
+//   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+//   use: {
+//     /* Base URL to use in actions like `await page.goto('/')`. */
+//     // baseURL: 'http://127.0.0.1:3000',
+
+//     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+//     trace: 'on-first-retry',
+//   },
+
+//   /* Configure projects for major browsers */
+// >>>>>>> 29a6d2ab26d1b0f6f982d91c2f4c52dd363c81cc
+//   projects: [
+//     {
+//       name: 'chromium',
+//       use: { ...devices['Desktop Chrome'] },
+//     },
+// <<<<<<< HEAD
+//     // {
+//     //   name: 'firefox',
+//     //   use: { ...devices['Desktop Firefox'] },
+//     // },
+//     // {
+//     //   name: 'webkit',
+//     //   use: { ...devices['Desktop Safari'] },
+//     // },
+//   ],
+ });
+ 
+ 
+// =======
+
+//     {
+//       name: 'firefox',
+//       use: { ...devices['Desktop Firefox'] },
+//     },
+
+//     {
+//       name: 'webkit',
+//       use: { ...devices['Desktop Safari'] },
+//     },
+
+//     /* Test against mobile viewports. */
+//     // {
+//     //   name: 'Mobile Chrome',
+//     //   use: { ...devices['Pixel 5'] },
+//     // },
+//     // {
+//     //   name: 'Mobile Safari',
+//     //   use: { ...devices['iPhone 12'] },
+//     // },
+
+//     /* Test against branded browsers. */
+//     // {
+//     //   name: 'Microsoft Edge',
+//     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+//     // },
+//     // {
+//     //   name: 'Google Chrome',
+//     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+//     // },
+//   ],
+
+//   /* Run your local dev server before starting the tests */
+//   // webServer: {
+//   //   command: 'npm run start',
+//   //   url: 'http://127.0.0.1:3000',
+//   //   reuseExistingServer: !process.env.CI,
+//   // },
+// });
+
+// >>>>>>> 29a6d2ab26d1b0f6f982d91c2f4c52dd363c81cc
+>>>>>>> 0a25a54 (ClubContact Invite)
